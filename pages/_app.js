31edd575cb453f778520fca6c2fb90c220d13ffe
PhenomenalTechs-hub <phenomenaltechs6080@gmail.com>
2021-07-13 15:35:04 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
 import "../styles/styles.scss";
-import Head from "next/head";
-import HeadScript from "../components/HeadScript";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <HeadScript />
       <Component {...pageProps} />;
     </>
   );
